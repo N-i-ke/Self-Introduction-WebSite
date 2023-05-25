c61f4aebdaca8e76dd02702f1a06ccc7c4b11cf6
N-i-ke <nike.forgithub@gmail.com>
2023-05-25 21:54:52 +0900

    change_react

--- selfweb/src/components/HomePage.jsx
+++ selfweb/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Sneaker from "../Image/works.png"; 
 
 const HomePage = () => {
 return (
@@ -6,7 +7,8 @@ return (
 <main id="main">
     <section id='video'>
         <video className='bg-video' src='../Video/TopQBeijing.mp4' autoPlay>ここにビデオタグが入ります。</video>
-      </section>
+    </section>
+    
       <section id="work">
         <figure className="text-container col2">
             <h2 className="sec-title text-container__title">
@@ -14,7 +16,131 @@ return (
                 My Work
             </span>
           </h2>
-        </figure>
+      </figure>
+      <div className='work-wrapper'>
+        <ul>
+          <li className='work-item animated-li'>
+            <figure>
+              <a href="/">
+                <img src={Sneaker} />
+                <figcaption>
+                  <div className='fig-inner'>
+                    <h3>
+                      動画付きカルーセル
+                    </h3>
+                    <p>
+                      カルーセルグリッドレイアウト
+                    </p>
+                    <p>
+                      制作期間：2日
+                    </p>
+                  </div>
+                </figcaption>
+              </a>  
+            </figure>
+          </li>
+          <li className='work-item animated-li'>
+            <figure>
+              <a href="/">
+                <img src="/" alt="/" />
+                <figcaption>
+                  <div className='fig-inner'>
+                    <h3>
+                      動画付きカルーセル
+                    </h3>
+                    <p>
+                      カルーセルグリッドレイアウト
+                    </p>
+                    <p>
+                      制作期間：2日
+                    </p>
+                  </div>
+                </figcaption>
+              </a>  
+            </figure>
+          </li>
+          <li className='work-item animated-li'>
+            <figure>
+              <a href="/">
+                <img src="/" alt="/" />
+                <figcaption>
+                  <div className='fig-inner'>
+                    <h3>
+                      動画付きカルーセル
+                    </h3>
+                    <p>
+                      カルーセルグリッドレイアウト
+                    </p>
+                    <p>
+                      制作期間：2日
+                    </p>
+                  </div>
+                </figcaption>
+              </a>  
+            </figure>
+          </li>
+          <li className='work-item animated-li'>
+            <figure>
+              <a href="/">
+                <img src="/" alt="/" />
+                <figcaption>
+                  <div className='fig-inner'>
+                    <h3>
+                      動画付きカルーセル
+                    </h3>
+                    <p>
+                      カルーセルグリッドレイアウト
+                    </p>
+                    <p>
+                      制作期間：2日
+                    </p>
+                  </div>
+                </figcaption>
+              </a>  
+            </figure>
+          </li>
+          <li className='work-item animated-li'>
+            <figure>
+              <a href="/">
+                <img src="/" alt="/" />
+                <figcaption>
+                  <div className='fig-inner'>
+                    <h3>
+                      動画付きカルーセル
+                    </h3>
+                    <p>
+                      カルーセルグリッドレイアウト
+                    </p>
+                    <p>
+                      制作期間：2日
+                    </p>
+                  </div>
+                </figcaption>
+              </a>  
+            </figure>
+          </li>
+          <li className='work-item animated-li'>
+            <figure>
+              <a href="/">
+                <img src="/" alt="/" />
+                <figcaption>
+                  <div className='fig-inner'>
+                    <h3>
+                      動画付きカルーセル
+                    </h3>
+                    <p>
+                      カルーセルグリッドレイアウト
+                    </p>
+                    <p>
+                      制作期間：2日
+                    </p>
+                  </div>
+                </figcaption>
+              </a>  
+            </figure>
+          </li>
+        </ul>
+      </div>
       </section>
     </main>
   );
