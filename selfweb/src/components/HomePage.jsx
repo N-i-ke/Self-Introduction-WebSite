50d3e26d36230e0f09d5af7f9d0b78704fbfa3a1
N-i-ke <nike.forgithub@gmail.com>
2023-05-27 14:48:17 +0900

    aboutコンテンツのhtml要素の追加

--- selfweb/src/components/HomePage.jsx
+++ selfweb/src/components/HomePage.jsx
@@ -17,9 +17,7 @@ return (
       <figure className="text-container col2">
         <h2 className="sec-title text-container__title">
           My Work
-          <span className="sub-sec-title">
-            My Work
-          </span>
+          <span className="sub-sec-title">My Work</span>
         </h2>
       </figure>
       <div className="work-wrapper">
@@ -164,12 +162,8 @@ return (
       <div className="profile">
         <div className="icon-wrapper">
           <h3 className="name animated">
-            <p className="name-main">
-              N-i-ke
-            </p>
-            <p className="name-sub">
-              N-i-ke
-            </p>
+            <p className="name-main">N-i-ke</p>
+            <p className="name-sub">N-i-ke</p>
           </h3>
         </div>
         <div className="about-wrapper">
@@ -177,9 +171,29 @@ return (
             <i className="fas fa-chess-king"></i>
             Personal History
           </h4>
+          <p className="animated">
+            <i className="fa-regular fa-dot-circle"></i>
+            2002年1月生まれ。東京都出身。
+            <br />
+            <i className="fa-regular fa-dot-circle"></i>
+            東京育ち、東京在住
+            <br />
+            <i className="fa-regular fa-dot-circle"></i>
+            趣味はテニスとハシゴ酒です
+            <br />
+          </p>
+          <h4 className="animated">
+            <i className="fas fa-chess-queen"></i>
+            Mind
+          </h4>
+          <p className="animated">
+            技術力向上に日々努力しておりますが、第一に双方すれ違いのない円滑なコミュニケーションを大事に活動しております。
+            業務においては「レスの速さ」を大切にしております。急なデザインの変更や修正なども、その都度しっかりと対応できるように心がけております。
+          </p>
         </div>
       </div>
     </section>
+    {/* image-sec */}
   </main>
 );
 }
