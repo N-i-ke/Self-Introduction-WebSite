76e81aac7796aa8ec0831897e33875eb570e1375
N-i-ke <nike.forgithub@gmail.com>
2023-05-27 16:08:53 +0900

    Serviceコンテンツの追加

--- selfweb/src/components/HomePage.jsx
+++ selfweb/src/components/HomePage.jsx
@@ -9,6 +9,7 @@ import Kadan from "../Image/kadan.png";
 import Sousaku from "../Image/sousaku.png"; 
 import Your from "../Image/your.png"; 
 
+
 const HomePage = () => {
 return (
   <main id="main">
@@ -194,6 +195,63 @@ return (
       </div>
     </section>
     {/* image-sec */}
+    <div className="image-sec"></div>
+    <section id="skill">
+      <h2 className="sec-title">
+        Skills
+        <span className="sub-sec-title">My Skills</span>
+      </h2>
+      <div className="skill-wrap wrapper animated">
+        <div className="skill-item animated-div">
+          <i className="fab fa-html5"></i>
+          <h4>HTML</h4>
+        </div>
+        <div className="skill-item animated-div">
+          <i className="fab fa-html5"></i>
+          <h4>CSS</h4>
+        </div>
+        <div className="skill-item animated-div">
+          <i className="fab fa-html5"></i>
+          <h4>SASS</h4>
+        </div>
+        <div className="skill-item animated-div">
+          <i className="fab fa-html5"></i>
+          <h4>JavaScript</h4>
+        </div>
+        <div className="skill-item animated-div">
+          <i className="fab fa-html5"></i>
+          <h4>PHP</h4>
+        </div>
+        <div className="skill-item animated-div">
+          <i className="fab fa-html5"></i>
+          <h4>WordPress</h4>
+        </div>
+      </div>
+    </section>
+    {/* service */}
+    <section id="service">
+      <h2 className="sec-title animated-div">
+        Service
+        <span className="sub-sec-title animated-li">My Service</span>
+      </h2>
+      <div className="service-wrapper">
+        <div className="service-wrap-left wrapper animated">
+          <i className="fas fa-edit animated"></i>
+          <h3 className="animated">コーポレートサイト制作</h3>
+          <p className="animated">
+            会社のイメージを重視しながら会社概要やお知らせの掲載、所在地地図の設置などに対応いたします。
+          </p>
+        </div>
+        <div className="service-wrap-right wrapper animated">
+          <i className="fas fa-laptop-code animated"></i>
+          <h3 className="animated">コーディング•WP構築</h3>
+          <p className="animated">
+            スマートフォンサイト、ワードプレスを使用した更新性の高いサイト、Javascriptを使用した動きのあるサイトなど、様々なサイトのコーディングを行っております。
+          </p>
+        </div>
+      </div>
+    </section>
+    {/* contact */}
   </main>
 );
 }
